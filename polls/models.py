21d0e31313ca3067362c0ac3d9a68d1dc5085d9b
AustinHull <adh86@dragons.drexel.edu>
2024-09-04 15:39:13 -0400

    Presenting some updated endpoints, configurations, and test/practice stubs.

--- polls/models.py
+++ polls/models.py
@@ -18,3 +18,13 @@ class Choice(models.Model):
 	def __str__(self):
 		return self.choice_text
 # Create your models here.
+class EstimatedWaitTime(models.Model):
+	etaMessage = models.CharField(max_length=200)
+	pub_date = models.DateTimeField("date published")
+	waitingTime = models.IntegerField(default=0)
+	def __str__(self):
+		return self.etaMessage
+	def getWaitEstimate(self):
+		return self.waitEstimate
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- polls/views.py
+++ polls/views.py
@@ -1,43 +1,66 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.template import loader
+from django.template.defaulttags import csrf_token
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect, requires_csrf_token, csrf_exempt, ensure_csrf_cookie
+from django.middleware.csrf import get_token
+import json
 
-from .models import Choice, Question
+from .models import Choice, Question, EstimatedWaitTime
 
-def index(request):
-	latest_question_list = Question.objects.order_by("-pub_date")[:5]
-	template = loader.get_template("polls/index.html")
-	context = {
-		"latest_question_list": latest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+	template_name = "polls/index.html"
+	context_object_name = "latest_wait_estimate"
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return EstimatedWaitTime.objects.order_by("-pub_date")
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+	model = EstimatedWaitTime
+	template_name = "polls/detail.html"
 
-def results(request, question_id):
-	waitTime = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/results.html", {"question": waitTime})
+class ResultsView(generic.DetailView):
+	model = EstimatedWaitTime
+	template_name = "polls/results.html"
 
-def vote(request, question_id):
-	latest_eta_ref_value = get_object_or_404(Question, pk=question_id)
+def post(request):
+	template_name = "polls/updateWaitTime/"
+	context_object_name = "polls_post_estimate"
 	try:
-		calculated_time = latest_eta_ref_value.choice_set.get(pk=request.POST["choice"])
+		responseDict = {"csrfmiddlewaretoken": request.META["CSRF_COOKIE"], "success": "false"}
+		print(responseDict)
+		if request.method == "POST":
+			reqBody = json.loads(request.body)
+			print("Body Data from POST req: ")
+			print(reqBody)
+			latest_eta_ref_value = get_object_or_404(EstimatedWaitTime, pk=1)
+			calculated_time = latest_eta_ref_value #.set(pk=request.POST["waitEstimate"])
+			calculated_time.waitingTime = reqBody['waitingTime']
+			calculated_time.save()
+			responseDict['waitingTime'] = calculated_time.waitingTime
+			responseDict['success'] = "true"
+			return JsonResponse(responseDict)
+		else:
+			print('csrf: ', request.META["CSRF_COOKIE"])
+			# Extra request-side processing in the context of this else-conditional...
+			latest_eta_ref_value = get_object_or_404(EstimatedWaitTime, pk=1)
+			responseDict['waitingTime'] = latest_eta_ref_value.waitingTime
+			return JsonResponse(responseDict)
 	except (KeyError, Choice.DoesNotExist):
 		# Redisplay question voting form
 		return render(
 			request,
-			"polls/detail.html",
+			"polls/index.html",
 			{
 				"latest_eta_ref_value": latest_eta_ref_value,
-				"error_message": "You didn't select a choice.",
+				"error_message": "Error with Server configuration at this time - please try again later.",
 			},
 		)
-	else:
-		calculated_time.votes = F("votes") + 1
-		calculated_time.save()
-		return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return JsonResponse({'csrf_token': csrf_token})
